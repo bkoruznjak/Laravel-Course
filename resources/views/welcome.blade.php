4e0df7813ab5e14684fe2cfbbe691a640b836f4a
bkoruznjak <borna.koruznjak@gmail.com>
2023-02-05 13:18:16 +0100

    Connect Alpine to show hide the modal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 <x-guest-layout>
-    <div class="flex flex-col bg-indigo-900 w-full h-screen">
+    <div
+        class="flex flex-col bg-indigo-900 w-full h-screen"
+        x-data="{
+            showSubscribe: false,
+
+        }"
+    >
         <nav class="flex flex-row pt-5 justify-between container mx-auto text-indigo-200">
             <a class="text-4xl font-bold" href="/">
                 <x-application-logo class="w-16 h-16 fill-current">
@@ -25,37 +31,52 @@
                     We are just checking the <span class="font-bold underline">TALL</span> stack. Would you mind
                     subscribing
                 </p>
-                <x-primary-button class="py-3 px-8 bg-red-500 hover:bg-red-600">
+                <x-primary-button
+                    class="py-3 px-8 bg-red-500 hover:bg-red-600"
+                    x-on:click="showSubscribe = true"
+                >
                     Subscribe
                 </x-primary-button>
             </div>
 
         </div>
-    </div>
 
-    <div class="flex flex-col bg-pink-400 w-full h-screen">
-        <p class="text-white text-5xl font-extrabold text-center">
-            Let's do it
-        </p>
 
-        <form action="" class="flex flex-col items-center p-24">
-            <x-text-input
-                class="px-5 py-3 w-80 border border-blue-400"
-                type="email"
-                name="email"
-                placeholder="Email address"
-            >
+        <div
+            class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
+            x-show="showSubscribe"
+            x-on:click.self="showSubscribe = false"
+            x-on:keydown.escape.window="showSubscribe = false"
+        >
 
-            </x-text-input>
+            <div class="m-auto bg-pink-400 shadow-2xl rounded-xl p-8">
+                <p class="text-white text-5xl font-extrabold text-center">
+                    Let's do it
+                </p>
 
-            <span class="text-gray-100 text-xs">
+                <form action="" class="flex flex-col items-center p-24">
+                    <x-text-input
+                        class="px-5 py-3 w-80 border border-blue-400"
+                        type="email"
+                        name="email"
+                        placeholder="Email address"
+                    >
+
+                    </x-text-input>
+
+                    <span class="text-gray-100 text-xs">
                 We will send you a confirmation email.
             </span>
 
-            <x-primary-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center">
-                Get In
-            </x-primary-button>
-        </form>
+                    <x-primary-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center">
+                        Get In
+                    </x-primary-button>
+                </form>
 
+            </div>
+
+        </div>
     </div>
+
+
 </x-guest-layout>
